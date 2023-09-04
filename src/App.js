4b5f339811734d6654ae5d904ad6fbafe0922c81
vikas nishad <vikasnishad.vn97@gmail.com>
2023-09-04 12:11:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Amenties from "./components/Amenties";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Register from "./components/Register";
+import SearchBar from "./components/SearchBar";
+import Book from "./components/Book";
+import Amenties2 from "./components/Amenties2";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+
+         
+     <Navbar/>
+     <SearchBar/>
+     <Amenties/>
+     <Contact/>
+     <Footer/>
+
+     <Register/>
+     <Book/>
+
+
     </div>
   );
 }
